501985b05f00e36e3ccc9c96057035c070dc6998
Patrik Jansson <patrik.ja@gmail.com>
2015-10-15 23:23:24 +0200

    More Chapter 4 reading and implementation testing

--- LearnHaskDA/Chapter4.hs
+++ LearnHaskDA/Chapter4.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE FlexibleContexts #-}
 module LearnHaskDA.Chapter4 where
 import Data.List
+import Data.Time
+import Data.Time.Clock
+import Data.Time.Calendar
 import Data.Convertible.Base
 import Database.HDBC.Sqlite3
 import Database.HDBC
 import Graphics.EasyPlot
 import LearnHaskDA.Chapter2
 
-readIntColumn    :: [[SqlValue]] -> Int -> [Integer]
+readUTCTimeColumn:: [[SqlValue]] -> Int -> [UTCTime]
+readIntegerColumn:: [[SqlValue]] -> Int -> [Integer]
 readDoubleColumn :: [[SqlValue]] -> Int -> [Double]
 readStringColumn :: [[SqlValue]] -> Int -> [String]
-readIntColumn    = readTypeColumn
+readUTCTimeColumn= readTypeColumn
+readIntegerColumn= readTypeColumn
 readDoubleColumn = readTypeColumn
 readStringColumn = readTypeColumn
 
@@ -20,5 +25,48 @@ readTypeColumn sqlResult index = map (fromSql . (!!index)) sqlResult
 queryDatabase :: FilePath -> String -> IO [[SqlValue]]
 queryDatabase databaseFile sqlQuery = do
   conn <- connectSqlite3 databaseFile
+  result <- quickQuery' conn sqlQuery []
+  disconnect conn
+  return result
+{-
+  -- The code in the book on page 60 is wrong:
+  let result = quickQuery' conn sqlQuery []
+  disconnect conn
+  result
+
+is the same as
+
   disconnect conn
   quickQuery' conn sqlQuery []
+
+which clearly disconnects before failing to do the query.
+
+-}
+
+pullStockClosingPrices :: FilePath -> String -> IO [(String, Double)]
+pullStockClosingPrices databaseFile database = do
+  sqlResult <- queryDatabase  databaseFile
+                              (  "SELECT date, adjclose"
+                               ++ " FROM " ++ database
+                               ++" ORDER BY date")
+  return $ zip  (readStringColumn sqlResult 0)
+                (readDoubleColumn sqlResult 1)
+
+testPlot = do
+  aapl <- pullStockClosingPrices "aapl.sql" "aapl"
+  plot (PNG "aapl.png")
+    $ Data2D [Title "AAPL", Style Lines] []
+    $ map (\(t, v) -> (time2year t, v))
+    $ filter (("2014-01-01"<=).fst) aapl
+
+-- Date conversion
+time2year :: String -> Double
+time2year t = fromIntegral (diffDays (parseDay t) startDay) / 365
+
+startDay :: Day
+startDay = fromGregorian 2014 01 01
+
+parseDay :: String -> Day
+parseDay = parseTimeOrError True defaultTimeLocale (iso8601DateFormat Nothing)
+
+-- Page 65
--- README.md
+++ README.md
@@ -14,3 +14,16 @@ sudo apt-get install libghc-hdbc-sqlite3-dev
 cabal install HDBC sqlite HDBC-sqlite3
 cabal configure
 ```
+
+## Chapter 3
+
+## Chapter 4
+
+```shell
+wget 'http://real-chart.finance.yahoo.com/table.csv?s=AAPL&d=10&e=10&f=2014&g=d&a=11&b=12&c=1980&ignore=.csv' -O aapl.csv
+```
+
+
+```haskell
+convertCSVFileToSQL "aapl.csv" "aapl.sql" "aapl" ["date STRING", "open REAL", "high REAL", "low REAL", "close REAL", "volume REAL", "adjclose REAL"]
+```
